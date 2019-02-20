2b76bf00623117a727f0f53cf2e07db4a141da30
rbtprograms <rbt61690@gmail.com>
2019-02-19 20:14:27 -0800

    gradebook coming along

---
+++ Gradebook/Gradebook/GradeBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook
+{
+    class GradeBook
+    {
+
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        private List<float> grades;
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ComputerStats()
+        {
+
+        }
+
+        public void ResetGrades()
+        {
+            grades.Clear();
+        }
+
+    }
+}
---
+++ Gradebook/Gradebook/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+
+            GradeBook book2 = book;
+            book2.AddGrade(75);
+        }
+    }
+}
