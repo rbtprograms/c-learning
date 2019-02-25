69fb55365bd70511d91fcfabeed5404a743f85a8
rbtprograms <rbt61690@gmail.com>
2019-02-25 06:46:47 -0800

    tests updated

---
+++ Gradebook/GradeBook.Test/Types/ReferenceTypesTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gradebook;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GradeBook.Test.Types
+{
+
+    [TestClass]
+    public class ReferenceTypesTests
+    {
+        [TestMethod]
+        public void VariablesHoldAReference()
+        {
+            Grades g1 = new Grades();
+            Grades g2 = g1;
+
+            g1.Name = "Bobbys gradebook";
+            Assert.AreEqual(g1.Name, g2.Name);
+        }
+    }
+}
--- Gradebook/Gradebook/Program.cs
+++ Gradebook/Gradebook/Program.cs
@@ -12,15 +12,6 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-
-            Grades g1 = new Grades();
-            Grades g2 = g1;
-
-            g1 = new Grades();
-            g1.Name = "Bobbys gradebook";
-            Console.WriteLine(g2.Name);
-
-            /*
             SpeechSynthesizer synth = new SpeechSynthesizer();
             synth.Speak("Hello and welcome to the grade book program.");
 
@@ -35,7 +26,6 @@ namespace Gradebook
             Console.WriteLine(stats.HighestGrade);
             Grades book2 = book;
             book2.AddGrade(75);
-            */
         }
     }
 }
