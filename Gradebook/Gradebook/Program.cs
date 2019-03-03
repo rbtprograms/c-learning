1fc10f06dcdc7586dfade90c5ce981a2c645bb21
rbtprograms <rbt61690@gmail.com>
2019-03-03 10:59:21 -0800

    refacotr and try catch finally

--- Gradebook/Gradebook/Program.cs
+++ Gradebook/Gradebook/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Speech.Synthesis;
+using System.IO;
 
 namespace Gradebook
 {
@@ -13,17 +14,52 @@ namespace Gradebook
         static void Main(string[] args)
         {
             Grades book = new Grades();
+            GetBookName(book);
+            AddGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
+        }
 
-            book.AddGrade(91);
-            book.AddGrade(89.5f);
-            book.AddGrade(75);
-            book.WriteGrades(Console.Out);
-
+        private static void WriteResults(Grades book)
+        {
             GradeStats stats = book.ComputeGrades();
             WriteResult("Average", stats.AverageGrade);
             WriteResult("Highest", stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
             WriteResult(stats.Description, stats.LetterGrade);
+        }
+
+        private static void SaveGrades(Grades book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+                outputFile.Dispose();
+            }
+        }
+
+        private static void AddGrades(Grades book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(Grades book)
+        {
+            try
+            {
+                Console.WriteLine("Enter a name");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Null Exception: ", ex.Message);
+            }
         }
 
         static void WriteResult(string description, string result)
